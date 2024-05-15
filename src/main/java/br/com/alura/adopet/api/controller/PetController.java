76fba89d62cab55ce58efd9202c63b0890e5345c
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-05-15 11:47:43 -0300

    refactor: implementing DTO pattern in PetService

--- src/main/java/br/com/alura/adopet/api/controller/PetController.java
+++ src/main/java/br/com/alura/adopet/api/controller/PetController.java
@@ -1,5 +1,6 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.dto.DadosDetalhesPet;
 import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.repository.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,15 +20,15 @@ public class PetController {
     private PetRepository repository;
 
     @GetMapping
-    public ResponseEntity<List<Pet>> listarTodosDisponiveis() {
+    public ResponseEntity<List<DadosDetalhesPet>> listarTodosDisponiveis() {
         List<Pet> pets = repository.findAll();
-        List<Pet> disponiveis = new ArrayList<>();
+        List<DadosDetalhesPet> dadosDetalhesPets = new ArrayList<>();
         for (Pet pet : pets) {
             if (pet.getAdotado() == false) {
-                disponiveis.add(pet);
+                dadosDetalhesPets.add(new DadosDetalhesPet(pet));
             }
         }
-        return ResponseEntity.ok(disponiveis);
+        return ResponseEntity.ok(dadosDetalhesPets);
     }
 
 }
